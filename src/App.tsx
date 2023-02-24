db71cafdd29a685f478d75da0f02bde8ab3bd69c
codeDulu <codeDulu@gmail.com>
2023-02-24 18:08:17 +0800

    feat：虚拟列表测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import '@/css/index.css';
 
-// import AutoScroll from '@/AutoScroll';
-
-import AutoScroll from '../lib/index';
-import '../lib/dist/index.css';
+import AutoScroll from './Components/index';
+// import AutoScroll from '../lib/index';
+// import '../lib/dist/index.css';
 
 // 发布版测试
 // import AutoScroll from 'du-autoscroll';
@@ -12,11 +12,14 @@ import '../lib/dist/index.css';
 function App() {
   return (
     <div className="App">
-      <AutoScroll isAuto={true}>
-        {
-          Array(24).fill('').map((_, index) => <div key={index}>{index}</div>)
-        }
-      </AutoScroll>
+      <div className='log'>
+        {<AutoScroll>
+          {
+            Array(20).fill('').map((_, index) => <div key={index}>{index}</div>)
+          }
+        </AutoScroll>}
+      </div>
+      <div className='footer'></div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import App2 from './App2';
 
 const root = document.getElementById('root');
 if(root) {
-  createRoot(root).render(<App />)
+  createRoot(root).render(<App2 />)
 }
